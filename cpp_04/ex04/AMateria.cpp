f9fce5ed066d7d6c932adad97195bdf38121696f
Hajar Ouaslam <houaslam@c4r8p3.1337.ma>
2023-08-25 10:14:07 +0100

    push

--- cpp_04/ex04/AMateria.cpp
+++ cpp_04/ex04/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: houaslam <houaslam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 12:56:06 by houaslam          #+#    #+#             */
-/*   Updated: 2023/08/24 14:03:37 by houaslam         ###   ########.fr       */
+/*   Updated: 2023/08/25 10:13:11 by houaslam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,22 @@ std::string const & AMateria::getType() const
 void AMateria::use(ICharacter& target)
 {
 	
+}
+
+AMateria::AMateria(){
+	std::cout << "AMateria Default constructor is called!\n";
+}
+
+AMateria::~AMateria(){
+	std::cout << "AMateria Destructor is called!\n";
+}
+
+AMateria::AMateria(AMateria &src){
+	*this = src;
+	std::cout << "AMateria copy constructor is called!\n";
+}
+
+AMateria& AMateria::operator=(AMateria &materia){
+	this->type = materia.type;
+	return *this;
 }
--- cpp_04/ex04/AMateria.hpp
+++ cpp_04/ex04/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: houaslam <houaslam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 12:56:03 by houaslam          #+#    #+#             */
-/*   Updated: 2023/08/24 14:14:50 by houaslam         ###   ########.fr       */
+/*   Updated: 2023/08/25 10:11:05 by houaslam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ protected:
 public:
     AMateria(std::string const & type);
     AMateria();
-	// [...]
+    ~AMateria();
+    AMateria(AMateria &src);
+	AMateria& operator=(AMateria &materia);
 	std::string const & getType() const; //Returns the materia type
 	virtual AMateria* clone() const = 0;
 	virtual void use(ICharacter& target);
