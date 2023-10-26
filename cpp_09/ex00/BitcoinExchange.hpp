6ae2aa1f7c5ab2c53bee9f09d406950796465126
Hajar Ouaslam <houaslam@e2r10p1.1337.ma>
2023-10-26 14:23:25 +0100

    push

---
+++ cpp_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,9 @@
+#ifndef BITCOINEXCHANGR_HPP
+#define BITCOINEXCHANGR_HPP
+
+#include <map>
+#include <iostream>
+#include <fstream>
+
+
+#endif
---
+++ cpp_09/ex00/main.cpp
@@ -0,0 +1,9 @@
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av){
+    if (ac == 2){
+        std::string format = "0000-00-00 | ";
+        std::ifstream file;
+        file.open(av[1]);
+    }
+}
