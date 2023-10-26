a1110f8b502721028da9fdcd9aa99a19a185b6c5
Hajar Ouaslam <houaslam@e2r10p1.1337.ma>
2023-10-26 15:10:23 +0100

    push

--- cpp_09/ex00/BitcoinExchange.hpp
+++ cpp_09/ex00/BitcoinExchange.hpp
@@ -4,6 +4,13 @@
 #include <map>
 #include <iostream>
 #include <fstream>
+#include <exception>
+
+class formatNotValid : public std::exception{
+    const char* what() const _NOEXCEPT{
+        return "format is not valid";
+    }
+};
 
 
 #endif
--- cpp_09/ex00/main.cpp
+++ cpp_09/ex00/main.cpp
@@ -1,9 +1,31 @@
 #include "BitcoinExchange.hpp"
 
+bool    checkFormat(std::string str){
+    std::string format = "0000-00-00 | ";
+	int k = 0;
+	while(k < 14){
+		if ((format[k] == '-' || format[k] == ' ' || format[k] == '|' ) && format[k] != str[k])
+			throw formatNotValid();
+		format[k] = str[k];
+        k++;
+	}
+    return true;
+}
+
 int main(int ac, char **av){
     if (ac == 2){
-        std::string format = "0000-00-00 | ";
+        std::string str;
         std::ifstream file;
         file.open(av[1]);
+        try{
+
+        while(std::getline(file, str, '\n')){
+            checkFormat(str);
+            std::cout << "GOOD\n";
+        }
+        }
+        catch(std::exception &e){
+            std::cout << e.what() << std::endl;
+        }
     }
 }
