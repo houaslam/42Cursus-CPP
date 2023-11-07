4aabe310d8c6310b9d3032de5c1aac79d46e5b16
Hajar Ouaslam <houaslam@c4r3p1.1337.ma>
2023-11-07 08:53:43 +0100

    push

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: houaslam <houaslam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 14:47:19 by houaslam          #+#    #+#             */
-/*   Updated: 2023/11/06 15:52:31 by houaslam         ###   ########.fr       */
+/*   Updated: 2023/11/07 08:53:34 by houaslam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,16 @@
 
 // INSERT SORT
 
+void swap(int& a, int &b){
+    int tp = b;
+    b = a;
+    a = tp;
+}
+
 void insert(std::vector<int>& small, std::vector<int>& big){
     for(size_t i=0; i <small.size(); i++){
-        big.insert(big.lower_bound(small[i]), small[i]);
+        big.insert(big.lower_bound(big.begin(), big.end() ,small[i]), small[i]);
+        // big.insert(big.lower_bound(small[i]), small[i]);
     }
 }
 
